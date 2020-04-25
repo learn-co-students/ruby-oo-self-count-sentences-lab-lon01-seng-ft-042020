00e360fac3e9edbaa04e71d4acd0510ab92585a3
Douglas <douglas@dobuilding.co.uk>
2020-04-25 18:18:02 +0100

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
-  end
+   if self.end_with?(".")
+    return true 
+   else 
+    false
+   end 
+  end 
+  
 
   def question?
+    if self.end_with?("?")
+      return true
+    else 
+      false
+    end 
 
   end
 
   def exclamation?
+    if self.end_with?("!")
+      return true
+    else 
+      false
+    end 
 
   end
 
   def count_sentences
-
+    self.squeeze('.!?').count('.!?')
+    
   end
 end
+
+
